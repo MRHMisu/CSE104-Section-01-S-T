6fb89673f9ca769d1206f11f3abfebb56c7baf27
MRHMisu <md.rakib.hossain.19@ucl.ac.uk>
2021-03-27 14:43:04 +0600

    unicode test

--- 0-quiz/quiz-section-02.-q1.c
+++ 0-quiz/quiz-section-02.-q1.c
@@ -5,11 +5,12 @@ int main()
     char input;
     scanf("%c", &input);
     if(input=='a'|| input=='A'||input=='e'|| input=='E'
-    ||input=='i'|| input=='I'||input=='o'|| input=='O'||input=='u'|| input=='U'){
-        printf("Requires Article An");
+    ||input=='i'|| input=='I'|| input=='o'|| input=='O' ||
+     input=='u'|| input=='U'){
 
+    printf("Requires Article 'An'.\n");
     }else{
-        printf("Requires Article A");
+        printf("Requires Article 'A'.\n");
     }
    
     return 0;
--- 0-quiz/quiz-section-02.-q2.c
+++ 0-quiz/quiz-section-02.-q2.c
@@ -4,11 +4,18 @@ int main()
 {
     int inital_amount, days;
     scanf("%d %d", &inital_amount, &days);
-    printf("%d", inital_amount<<days);// executed 1 instruction
 
-    for(int i=0; i<days; i++)// 6 times+ 6 times       
-        inital_amount*=2;// 12 times // executed 24 instruction  
-    printf("%d\n",inital_amount);
+    // considering 10 6
+    for (int i = 0; i < days; i++)// 1+ (6*2)+6+6=25 instructions
+        inital_amount *=2;
+    printf("%d", inital_amount);
 
+
+   // printf("%d", inital_amount<<days);// 1 instruction 
+   // 3.2x10^9 Hz
+   // 320 can be executable per second 
+
+
+   
     return 0;
 }
